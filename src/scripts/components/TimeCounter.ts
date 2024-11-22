3d2667c88d0d85afc1ed185f77db5d9095d690d6
s1rdev10us <90798437+s1rdev10us@users.noreply.github.com>
2024-11-22 11:22:36 +0000

    fix: add placeholder text to Countdown in case scripts are disabled

--- src/scripts/components/TimeCounter.ts
+++ src/scripts/components/TimeCounter.ts
@@ -1,39 +1,57 @@
 import moment from "moment";
-class TimeCountdown extends HTMLElement {
-	#callback?: number | undefined;
+export function stringFromDate(
+	date: Date,
+	pastText: string = "Released",
+	futureText: string = "Releases",
+) {
+	let onServer = import.meta.env.SSR;
+	let momentDate = moment(date);
 
-	constructor() {
-		super();
-		this.updateTime();
-		this.connectedCallback();
-	}
-	connectedCallback() {
-		if (this.#callback) {
+	return `${momentDate.isBefore() ? pastText : futureText}\xa0${
+		onServer ? "~" : ""
+	}${momentDate.fromNow().replaceAll(" ", "\xa0")}`;
+}
+
+if (!import.meta.env.SSR) {
+	class TimeCountdown extends HTMLElement {
+		#callback?: number | undefined;
+
+		constructor() {
+			super();
+			this.updateTime();
+			this.connectedCallback();
+		}
+		connectedCallback() {
+			if (this.#callback) {
+				clearInterval(this.#callback);
+				this.#callback = undefined;
+			}
+			this.#callback = setInterval(
+				this.updateTime.bind(this),
+				60000,
+				undefined,
+			);
+		}
+		disconnectedCallback() {
 			clearInterval(this.#callback);
 			this.#callback = undefined;
 		}
-		this.#callback = setInterval(this.updateTime.bind(this), 60000, undefined);
-	}
-	disconnectedCallback() {
-		clearInterval(this.#callback);
-		this.#callback = undefined;
-	}
-	attributeChangedCallback() {
-		this.updateTime();
-	}
-	static get observedAttributes() {
-		return ["data-date"];
-	}
-	updateTime() {
-		const d = this.dataset.date;
-		if (!d) return;
-		let momentDate = moment(new Date(+d));
+		attributeChangedCallback() {
+			this.updateTime();
+		}
+		static get observedAttributes() {
+			return ["data-date"];
+		}
+		updateTime() {
+			const d = this.dataset.date;
+			if (!d) return;
 
-		this.innerText = `${
-			momentDate.isBefore()
-				? this.dataset.pastText ?? "Released"
-				: this.dataset.futureText ?? "Releases"
-		}\xa0${momentDate.fromNow().replaceAll(" ", "\xa0")}`;
+			this.innerText = stringFromDate(
+				new Date(isNaN(+d) ? d : +d),
+				this.dataset.pastText,
+				this.dataset.futureText,
+			);
+		}
 	}
+	customElements.define("time-counter", TimeCountdown);
 }
-customElements.define("time-counter", TimeCountdown);
